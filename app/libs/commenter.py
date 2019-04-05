dd68578649a63855c47148d69dbd8084ea7e2696
Gaurav <gaurav@scroll.in>
2019-04-05 16:21:08 +0530

    Commenter lib

---
+++ app/libs/commenter.py
@@ -0,0 +1,34 @@
+import logging
+
+from app.models import Commenter
+
+
+def create(uid, username, name, bio, web):
+    commenter = Commenter.create(uid=uid, username=username, name=name, bio=bio, web=web)
+    return commenter.id
+
+
+def get(id):
+    commenter = Commenter.select().where(Commenter.id == id).first()
+    return commenter.to_dict() if commenter else None
+
+
+def update(id, mod_data):
+    updatables = set((
+        'uid',
+        'username',
+        'name',
+        'enabled',
+        'badges',
+        'bio',
+        'web',
+        'verified'
+    ))
+    if not updatables.issuperset(mod_data.keys()):
+        raise Exception('not possible')
+
+    Commenter.update(**mod_data).where(Commenter.id == id).execute()
+
+
+def delete(id):
+    Commenter.delete().where(Commenter.id == id).execute()
---
+++ tests/test_commenter.py
@@ -0,0 +1,41 @@
+from tests.data import test_commenter
+from app.models import setup_db, destroy_db
+from app.libs import commenter as commenterlib
+
+
+def test_suite_setup():
+    destroy_db()
+    setup_db()
+
+
+def test_create():
+    commenter_id = commenterlib.create(**test_commenter)
+    assert commenter_id == 1
+
+
+def test_get():
+    commenter = commenterlib.get(1)
+    assert commenter["id"] == 1
+    assert test_commenter.items() < commenter.items()
+
+
+def test_update():
+    mod_data = {"id": 1}
+    try:
+        commenterlib.update(1, mod_data)
+    except:
+        pass
+    else:
+        assert False
+
+    mod_data = {"bio": "new_bio"}
+    commenterlib.update(1, mod_data)
+    commenter = commenterlib.get(1)
+    assert commenter["id"] == 1
+    assert commenter["bio"] == mod_data["bio"]
+
+
+def test_delete():
+    commenterlib.delete(1)
+    commenter = commenterlib.get(1)
+    assert commenter is None
