2306ab42d72a19be029f595305a2d2ba1e54c655
Gaurav <gaurav@scroll.in>
2019-04-05 12:40:44 +0530

    Publication lib

---
+++ app/libs/publication.py
@@ -0,0 +1,31 @@
+from app.models import Publication
+
+
+def create(name, host):
+    publication = Publication.create(name=name, host=host)
+    return publication.id
+
+
+def get(id):
+    publication = Publication.select().where(Publication.id == id).first()
+    return publication.to_dict() if publication else None
+
+
+def get_all():
+    publications = Publication.select().execute()
+    return [publication.to_dict() for publication in publications]
+
+
+def update(id, mod_data):
+    updatables = set((
+        'name',
+        'host'
+    ))
+    if not updatables.issuperset(mod_data.keys()):
+        raise Exception('not possible')
+
+    Publication.update(**mod_data).where(Publication.id == id).execute()
+
+
+def delete(id):
+    Publication.delete().where(Publication.id == id).execute()
