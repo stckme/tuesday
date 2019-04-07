876f883b29b23c40387ab2bc5950b00d9578eeeb
Shekhar <pythonic@gmail.com>
2019-04-07 19:40:12 +0530

    REST mapping

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,7 +1,11 @@
 import app.libs.debug as debughelpers
+import app.libs.asset_request as arlib
 
 
 def setup_routes(factory):
 
     factory.get('/echo/{something}')(debughelpers.echo)
     factory.post('/echo')(debughelpers.echo)
+
+    ar_handlers = (None, arlib.create, None, arlib.get, arlib.update, None)
+    factory.map_resource('/resttest/', handlers=ar_handlers)
