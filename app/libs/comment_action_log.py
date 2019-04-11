f259a8389d10fa4db53c068ff62c96d91fa0011b
Gaurav <gaurav@scroll.in>
2019-04-11 11:53:54 +0530

    added file

---
+++ app/libs/comment_action_log.py
@@ -0,0 +1,23 @@
+from app.models import CommentActionLog
+
+
+def create(comment, action, actor):
+    action_log = CommentActionLog.create(
+        comment=comment,
+        action=action,
+        actor=actor
+    )
+    return action_log.id
+
+
+def list_(page=1, size=20):
+    action_logs = CommentActionLog.select().order_by(CommentActionLog.created.desc()).paginate(page, size)
+    return [log.to_dict() for log in action_logs]
+
+
+def list_by_comment(comment, page=1, size=20):
+    action_logs = CommentActionLog.select().where(
+        CommentActionLog.comment==comment
+        ).order_by(CommentActionLog.created.desc()
+        ).paginate(page, size)
+    return [log.to_dict() for log in action_logs]
