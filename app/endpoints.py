1997a613d61a81aa79afddcb96be8e1d5253a152
Shekhar <pythonic@gmail.com>
2019-04-25 15:23:20 +0530

    session debugging helper endpoint

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,10 +1,12 @@
 import app.libs.debug as debughelpers
 import app.libs.asset_request as arlib
+import apphelpers.sessions as sessionlib
 
 
 def setup_routes(factory):
 
     factory.get('/echo/{word}')(debughelpers.echo)
+    factory.get('/whoami')(sessionlib.whoami)
 
     ar_handlers = (None, arlib.create, None, arlib.get, arlib.update, None)
     factory.map_resource('/assets/', handlers=ar_handlers)
