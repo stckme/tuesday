2b7a20f7fb31a71b118b3cf33aee7bb68c1c7061
Anjali Pardeshi <anjali_p@scroll.in>
2019-11-28 17:32:35 +0530

    user email

--- app/libs/pending_comment.py
+++ app/libs/pending_comment.py
@@ -78,7 +78,7 @@ def update(id, actor: user_id, **mod_data):
 update.groups_required = [groups.moderator.value]
 
 
-def approve(id, actor: user_id, actor_email: user_email):
+def approve(id, actor: user_id):
     pending_comment = get(id)
     delete(id)
     commentactionloglib.create(
@@ -87,6 +87,8 @@ def approve(id, actor: user_id, actor_email: user_email):
         actor=actor
     )
     if settings.EMAIL_NOTIFICATION:
+        commenter = memberlib.get(pending_comment['commenter_id'])
+
         comment = pending_comment['content']
         display_comment = comment[:215] + '...' if len(comment) > 215 else comment
 
@@ -98,7 +100,7 @@ def approve(id, actor: user_id, actor_email: user_email):
                 comment_id=id
             )
         )
-        signals.send_notification.send((actor_email,), **email_info)
+        signals.send_notification.send((commenter['email'],), **email_info)
     return commentlib.create(**pending_comment)
 approve.groups_required = [groups.moderator.value]
 
