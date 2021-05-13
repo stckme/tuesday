bb84168e69b54fafedc8cf6cd5dc32388cd4e881
Mandar Vaze <mandarvaze@gmail.com>
2021-05-13 11:26:30 +0530

    Add command to delete a member

--- app/cli.py
+++ app/cli.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 
 """Console script for tuesday."""
-import sys
 import click
 
+from app.libs import member as memberlib
 
-@click.command()
-def main(args=None):
-    """Console script for tuesday."""
-    click.echo("Replace this message by putting your code into "
-               "tuesday.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
 
+@click.group()
+def cli_group():
+    pass
+
+
+@cli_group.command()
+@click.option('--email', prompt='email',
+              help='The email of the Member to delete.')
+def delete_user(email):
+    """Delete a user"""
+    member = memberlib.get_by_email(email)
+    if member:
+        memberlib.delete(member['id'])
+        print(f"Member with the email : {email} - deleted successfully.")
+    else:
+        print(f"Member with the email : {email} - not found.")
+
+
+cli = click.CommandCollection(sources=[cli_group])
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    cli()
